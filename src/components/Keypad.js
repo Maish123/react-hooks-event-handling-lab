083b477d6e99c38dc18f21d1014d69a12329b5f6
Denis Maina <Denis.maina@student.moringaschool.com>
2022-07-17 14:13:06 +0300

    initial commit

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
+import React from "react";
 // Code Keypad Component Here
+
+function Keypad(){
+    function handleChange(){
+        console.log('Entering password...')
+    }
+
+    return(
+        <input
+        type="password"
+        name="password"
+        onChange={handleChange}
+        placeholder="enter password"
+        />
+    )
+}
+
+export default Keypad;
